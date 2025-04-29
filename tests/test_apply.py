3cf91930c641bb6bcc9f04528b13c1c52a6f0636
Hannes Bruger <bruger@team23.de>
2025-04-29 15:43:50 +0200

    fix: Undo and explain changes

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -186,8 +186,9 @@ def test_apply_will_handle_inner_apply_model():
 
 
 def test_apply_will_handle_inner_apply_model_with_validate_assignment():
-    inner_with_apply = InnerWithApplyModel(a=1)
-    obj = ApplyModelWithValidation(a=1, b=2, inner_with_apply=inner_with_apply)
+    obj = ApplyModelWithValidation(a=1, b=2, inner_with_apply=InnerWithApplyModel(a=1))
+    # Get the value of inner_with_apply afterwards because it might already have changed once
+    inner_with_apply = obj.inner_with_apply
 
     obj.model_apply({
         "inner_with_apply": PatchModel(b=1),
